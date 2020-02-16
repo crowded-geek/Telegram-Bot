b3080084319051991c9b1334a55c7f7fd274f0bc
sayam <sawaisayam@gmail.com>
2020-02-16 22:26:44 +0530

    Created Bot

---
+++ index.js
@@ -0,0 +1,24 @@
+const TelBot = require('node-telegram-bot-api')
+
+const token = '1042222855:AAF6dg320_lk54JLXgUbV1OEtNdEogMF5DQ'
+const bot = new TelBot(token,{polling:true});
+
+// Variations of Nice
+const variations = ['Nice',,'Nicee','Noice','ładny','niice','Obligatory Nice']
+
+// On New Message
+bot.on('message',(msg) => {
+
+    if(msg.text == undefined){
+        return
+    }
+
+    const newMsg = msg.text
+    
+    // Send if text contains 69
+    if(newMsg.includes('69')){
+        // Select Random variation of Nice
+        const randomNice = variations[Math.floor(Math.random()*variations.length)]
+        bot.sendMessage(msg.chat.id, randomNice);
+    }
+});
